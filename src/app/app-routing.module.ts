3aed56f9d7ce37fb72cdeaf206f6160f13abb8a0
J. Mario Rguez <juan.rodriguezp@atos.net>
2020-07-04 09:10:04 +0200

    Add routing module and router-outlet

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { LoginComponent } from './login/login.component';
+import { MenuComponent } from './menu/menu.component';
+
+
+const routes: Routes = [
+  { path: 'login', component: LoginComponent},
+  { path: 'menu', component: MenuComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
