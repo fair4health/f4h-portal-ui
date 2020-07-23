6943c1304c2e6bc481bc324a42d251df4fc1f5e5
J. Mario Rguez <juan.rodriguezp@atos.net>
2020-07-23 22:18:01 +0200

    Include logout dialog

---
+++ src/app/header/logout-dialog/logout-dialog.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+
+@Component({
+  selector: 'app-logout-dialog',
+  templateUrl: './logout-dialog.component.html',
+  styleUrls: ['./logout-dialog.component.css']
+})
+export class LogoutDialogComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
