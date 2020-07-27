586e86e5d7da0220e08e38e21ca8eecbe58031d8
J. Mario Rguez <juan.rodriguezp@atos.net>
2020-07-27 23:44:24 +0200

    Add auth service functionality for header and login

--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -1,9 +1,34 @@
 import { Injectable } from '@angular/core';
+import { BackendService } from './backend.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  username: string;
+  token: string;
+
+  constructor(
+    private backendService: BackendService
+  ) { }
+
+  isLoggedIn(): boolean {
+    if (this.username !== undefined) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  login(user: string, token: string): void {
+    this.username = user;
+    this.token = token;
+  }
+
+  logout(): void {
+    this.username = undefined;
+    this.token = undefined;
+  }
+
 }
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { AuthService } from '../core/services/auth.service';
+import { BackendService } from '../core/services/backend.service';
+import { UserCommunicationService } from '../core/services/user-communication.service';
 
 @Component({
   selector: 'app-login',
@@ -8,17 +13,36 @@ import { Component, OnInit } from '@angular/core';
 export class LoginComponent implements OnInit {
   hide = true;
 
-  constructor() { }
+  constructor(
+    private router: Router,
+    public auth: AuthService,
+    private backendService: BackendService,
+    private userCommunication: UserCommunicationService
+    ) { }
 
   ngOnInit(): void {
   }
 
-  rememberPassword(): void {
-    console.log('Clicked rememberPassword. TODO: remember password');
+  onRememberPassword(): void {
+    console.log('Clicked remember password. TODO: remember password');
   }
 
-  askAccess(): void {
-    console.log('Clicked askAccess. TODO: user creation');
+  onAskAccess(): void {
+    console.log('Clicked ask access. TODO: user creation');
+  }
+
+  onLogin(user: string, pass: string): void {
+    console.log('Clicked login submit');
+    this.backendService.login(user, pass).subscribe(
+      (data) => {
+        console.log('Login answer received!');
+        this.auth.login(user, data.access_token);
+        this.router.navigate(['/uclist']);
+      },
+      (err) => {
+        this.backendService.handleError('home', err);
+        this.userCommunication.createMessage('Login failed!', this.userCommunication.ERROR);
+      });
   }
 
 }
