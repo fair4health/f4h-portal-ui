068dbe5246c3f45a87c81945ff191eb56c1d3cd0
J. Mario Rguez <juan.rodriguezp@atos.net>
2020-07-28 10:07:41 +0200

    Update use case menu with router links

--- src/app/use-case-menu/use-case-menu.component.ts
+++ src/app/use-case-menu/use-case-menu.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 
 @Component({
@@ -10,9 +11,26 @@ export class UseCaseMenuComponent implements OnInit {
 
   name = 'Discovery of disease association patterns in comorbid patients';
   description = 'Translates into economic saving related to the reduction of efforts in parallel research, and impacts on health outcomes related to the managements of this kind of patients.';
-  constructor() { }
+
+  constructor(
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
   }
 
+  onFeatureMgmt(): void {
+    console.log('Feature management');
+    this.router.navigate(['/fslist']);
+  }
+
+  onDataSetMgmt(): void {
+    console.log('Data Set management');
+    this.router.navigate(['/dsdashboard']);
+  }
+
+  onModelMgmt(): void {
+    console.log('Model management');
+    this.router.navigate(['/mdashboard']);
+  }
 }
