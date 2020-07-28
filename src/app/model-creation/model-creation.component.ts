3554d87b975855656eeb0ea8bff75498f69fb4c0
J. Mario Rguez <juan.rodriguezp@atos.net>
2020-07-28 14:59:56 +0200

    Add model creation stepper

--- src/app/model-creation/model-creation.component.ts
+++ src/app/model-creation/model-creation.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-model-creation',
@@ -6,10 +7,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./model-creation.component.css']
 })
 export class ModelCreationComponent implements OnInit {
+  formGroup1: FormGroup;
+  formGroup2: FormGroup;
+  formGroup3: FormGroup;
+  formGroup4: FormGroup;
+  formGroup5: FormGroup;
+  formGroup6: FormGroup;
 
-  constructor() { }
+
+  constructor(
+    private formBuilder: FormBuilder
+    ) {}
 
   ngOnInit(): void {
+    this.formGroup1 = this.formBuilder.group({
+      formGroup1: ['', Validators.required]
+    });
+    this.formGroup2 = this.formBuilder.group({
+      formGroup2: ['', Validators.required]
+    });
+    this.formGroup3 = this.formBuilder.group({
+      formGroup3: ['', Validators.required]
+    });
+    this.formGroup4 = this.formBuilder.group({
+      formGroup4: ['', Validators.required]
+    });
+    this.formGroup5 = this.formBuilder.group({
+      formGroup5: ['', Validators.required]
+    });
+    this.formGroup6 = this.formBuilder.group({
+      formGroup5: ['', Validators.required]
+    });
   }
-
 }
