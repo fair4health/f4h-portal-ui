03d7109c560894cc9a08917ccbe99e9414e982c3
J. Mario Rguez <juan.rodriguezp@atos.net>
2020-07-30 11:07:10 +0200

    Create tables for model dashboard

--- src/app/model-dashboard/model-dashboard.component.ts
+++ src/app/model-dashboard/model-dashboard.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, QueryList, ViewChildren } from '@angular/core';
+
+import { MatTable } from '@angular/material/table';
+
+import { BackendService } from '../core/services/backend.service';
+import { UserCommunicationService } from '../core/services/user-communication.service';
 
 @Component({
   selector: 'app-model-dashboard',
@@ -7,9 +12,57 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ModelDashboardComponent implements OnInit {
 
-  constructor() { }
+  @ViewChildren(MatTable) table !: QueryList<MatTable<string>>;
+
+  displayedColumns: string[] = ['name', 'description', 'algorithms', 'dataset_sources', 'execution_state', 'created_by', 'created_on', 'details'];
+  dataSourceReady = [];
+  dataSourceInProgress = [];
+
+  constructor(
+    private backendService: BackendService,
+    private userCommunication: UserCommunicationService
+    ) { }
 
   ngOnInit(): void {
+    this.getModelsList();
   }
 
+  getModelsList(): void {
+    this.backendService.getModelsList().subscribe(
+      (modelslist) => {
+        console.log(modelslist);
+        modelslist.forEach(element => {
+          let algorithmsList = '';
+          element.algorithms.forEach(innerElement => {
+            algorithmsList = algorithmsList + innerElement.name + ' ';
+          });
+          element.algorithms = algorithmsList;
+
+          let datasetSourcesList = '';
+          element.dataset.dataset_sources.forEach(innerElement => {
+            datasetSourcesList = datasetSourcesList + innerElement.data_source.name + ' ';
+          });
+          element.dataset = datasetSourcesList;
+
+          if (element.execution_state === 'final') {
+            this.dataSourceReady.push(element);
+          } else {
+            this.dataSourceInProgress.push(element);
+          }
+          console.log('Model updated in table sources. ' + this.dataSourceReady.length + ' and ' + this.dataSourceInProgress.length + ' rows');
+        });
+        this.table.first.renderRows();
+        this.table.last.renderRows();
+      },
+      (err) => {
+        this.backendService.handleError('home', err);
+        this.userCommunication.createMessage(this.userCommunication.ERROR, 'Get feature set list operation failed');
+      });
+  }
+
+  onSelectModel(selectedModel): void {
+    console.log('The selected model is: ' + selectedModel);
+    // TO DO Feature set details dialog
+    this.userCommunication.createMessage(this.userCommunication.INFO, 'Not ready yet');
+  }
 }
