a9a46abebce4f811a8cc8d638c3e9d84829970d1
Quintero Padrón <miriam.quintero@atos.net>
2021-01-14 18:43:40 +0000

    add loginClinical to manage clinical role add registerd role and how it on interface

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,8 +7,9 @@ export const environment = {
 //  restApiUrl: 'http://localhost:3000/'
   restApiUrl: 'https://f4h-portal.ari-health.eu/',
   restApiPPDDM: 'https://f4h.srdc.com.tr/',
-  loginOAUth: 'https://testbed.fair4health.eu/oauth/',
-  // loginOAUth: 'http://localhost:8081/'
+  // loginOAUth: 'https://testbed.fair4health.eu/oauth/login/',
+  // loginOAUth: 'http://localhost:8081/login/'
+  loginOAUth: 'http://localhost:3000/'
 };
 
 /*
