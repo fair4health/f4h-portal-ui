a8a0cd933b2557984a0d5894bb7363b44c950da2
Bruno Lima <alexander.bruno@atos.net>
2021-02-12 12:07:17 +0100

    Dialog confirmation of close the form in feature set

---
+++ src/app/dialog-confirmation/dialog-confirmation.component.ts
@@ -0,0 +1,24 @@
+import { Component, Inject, OnInit } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+
+@Component({
+  selector: 'app-dialog-confirmation',
+  templateUrl: './dialog-confirmation.component.html',
+  styleUrls: ['./dialog-confirmation.component.css']
+})
+export class DialogConfirmationComponent implements OnInit {
+
+  // data: the text of the dialog.
+  constructor(
+    public dialogRef: MatDialogRef<DialogConfirmationComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: any
+  ) { }
+
+  ngOnInit(): void {
+  }
+
+  onNoClick(): void {
+    this.dialogRef.close();
+  }
+
+}
